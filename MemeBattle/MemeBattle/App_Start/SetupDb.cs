a967fdd98c5d03d46f35de6b04c1e26c8f3fdb77
Matt <m4tt1muspr1m3@gmail.com>
2013-03-17 20:09:15 -0700

    fixed issue with multiples of same meme in db

--- MemeBattle/MemeBattle/App_Start/SetupDb.cs
+++ MemeBattle/MemeBattle/App_Start/SetupDb.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MemeBattle.Controllers;
 using Raven.Client;
 using MemeBattle.Models;
 
@@ -14,98 +15,82 @@ namespace MemeBattle.App_Start
         {
             // TODO this is an anti-pattern, fix it later
             IDocumentSession session = DependencyResolver.Current.GetService<IDocumentSession>();
-
+            DeleteExistingMemes(session);
             PutMemesInDb(session);
         }
 
         private static void PutMemesInDb(IDocumentSession session)
         {
-            // TODO add hashtags
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Crazy Girl")))
-            {
-                Meme crazyGirl = new Meme()
-                    {
-                        Id = "CrazyGirl",
-                        DisplayName = "CrazyGirl",
-                        PhotoName = "CrazyGirl.jpg",
-                        HashTag = "#MemeBattlesCrazyGirl"
-                    };
-                session.Store(crazyGirl);
-            }
+            // Add default memes
+            Meme crazyGirl = new Meme()
+                {
+                    DisplayName = "CrazyGirl",
+                    PhotoName = "CrazyGirl.jpg",
+                    HashTag = "#MemeBattlesCrazyGirl"
+                };
+            session.Store(crazyGirl);
 
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Cyclist Cat")))
-            {
-                Meme cyclistCat = new Meme()
-                    {
-                        Id = "CyclistCat",
-                        DisplayName = "Cyclist Cat",
-                        PhotoName = "CyclistCat.jpg",
-                        HashTag = "#MemeBattlesCyclistCat"
-                    };
-                session.Store(cyclistCat);
-            }
+            Meme cyclistCat = new Meme()
+                {
+                    DisplayName = "Cyclist Cat",
+                    PhotoName = "CyclistCat.jpg",
+                    HashTag = "#MemeBattlesCyclistCat"
+                };
+            session.Store(cyclistCat);
 
-            //session.Advanced.DocumentStore.DatabaseCommands.Delete("Memes/Elyse");
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Elyse")))
+            Meme elyse = new Meme()
             {
-                Meme elyse = new Meme()
-                    {
-                        Id = "Elyse",
-                        DisplayName = "Elyse",
-                        PhotoName = "ElyseQuit.jpg",
-                        HashTag = "#MemeBattlesElyse"
-                    };
-                session.Store(elyse);
-            }
+                DisplayName = "Elyse",
+                PhotoName = "ElyseQuit.jpg",
+                HashTag = "#MemeBattlesElyse"
+            };
+            session.Store(elyse);
 
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Hitler Cat")))
+
+            Meme hitlerCat = new Meme()
+                {
+                    DisplayName = "Hilter Cat",
+                    PhotoName = "HitlerCat.jpg",
+                    HashTag = "#MemeBattlesHitlerCat"
+                };
+            session.Store(hitlerCat);
+
+            Meme honeyBadger = new Meme()
+                {
+                    DisplayName = "Honey Badger",
+                    PhotoName = "HoneyBadger.gif",
+                    HashTag = "#MemeBattlesHoneyBadger"
+                };
+            session.Store(honeyBadger);
+
+            Meme limeCat = new Meme()
+                {
+                    DisplayName = "Lime Cat",
+                    PhotoName = "LimeCat.jpg",
+                    HashTag = "#MemeBattlesLimeCat"
+                };
+            session.Store(limeCat);
+
+            Meme lulzSec = new Meme()
+                {
+                    DisplayName = "Lulz Sec",
+                    PhotoName = "LulzSec.jpg",
+                    HashTag = "#MemeBattlesLulzSec"
+                };
+            session.Store(lulzSec);
+
+            session.SaveChanges();
+        }
+
+        private static void DeleteExistingMemes(IDocumentSession session)
+        {
+            MemeApiController c = new MemeApiController(session);
+            List<Meme> memes = c.GetAll();
+            foreach (var meme in memes)
             {
-                Meme hitlerCat = new Meme()
-                    {
-                        Id = "HitlerCat",
-                        DisplayName = "Hilter Cat",
-                        PhotoName = "HitlerCat.jpg",
-                        HashTag = "#MemeBattlesHitlerCat"
-                    };
-                session.Store(hitlerCat);
+                string id = session.Advanced.GetDocumentId(meme);
+                session.Advanced.DocumentStore.DatabaseCommands.Delete(id, null);
             }
-
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Honey Badger")))
-            {
-                Meme honeyBadger = new Meme()
-                    {
-                        Id = "HoneyBadger",
-                        DisplayName = "Honey Badger",
-                        PhotoName = "HoneyBadger.gif",
-                        HashTag = "#MemeBattlesHoneyBadger"
-                    };
-                session.Store(honeyBadger);
-            }
-
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Lime Cat")))
-            {
-                Meme limeCat = new Meme()
-                    {
-                        Id = "LimeCat",
-                        DisplayName = "Lime Cat",
-                        PhotoName = "LimeCat.jpg",
-                        HashTag = "#MemeBattlesLimeCat"
-                    };
-                session.Store(limeCat);
-            }
-
-            if (!session.Query<Meme>().Any(x => x.Id.Equals("Lulz Sec")))
-            {
-                Meme lulzSec = new Meme()
-                    {
-                        Id = "LulzSec",
-                        DisplayName = "Lulz Sec",
-                        PhotoName = "LulzSec.jpg",
-                        HashTag = "#MemeBattlesLulzSec"
-                    };
-                session.Store(lulzSec);
-            }
-
             session.SaveChanges();
         }
     }
